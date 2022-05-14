84757ba1d03598a413b11676496a3b24694cc6cb
“weimingxiang” <1578270912@qq.com>
2022-05-14 16:53:57 +0800

    commit init 0

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
-from m_albert import AlbertModel
 import torch
-model = AlbertModel.from_pretrained("albert-base-v2")
+from multiprocessing import Pool, cpu_count
+
+data_dir = "../datasets/NA12878_PacBio_MtSinai/"
+
+pool = Pool(2)
+
+all_p_img, all_n_img = pool.map(torch.load, [data_dir + '/all_p_img' + '.pt', data_dir + '/all_n_img' + '.pt'])
+# all_p_list, all_n_list = pool.map(torch.load, [data_dir + '/all_p_list' + '.pt', data_dir + '/all_n_list' + '.pt'])
 
 
-a = torch.rand(10, 13)
-b = torch.rand(20, 13)
-c = torch.rand(15, 13)
-input_ids = [a, b, c]
+hight = 224
 
-model(input_ids,
-       attention_mask=torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]),
-       token_type_ids=torch.tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]),
-       # encoder_attention_mask=mask,
-       output_hidden_states=True,
-       return_dict=False)
+a = torch.empty(22199, 3+3+4, hight, hight)
+b = torch.empty(22199, 3+3+4, hight, hight)
+# c = torch.empty(2000, 512, 9)
+# d = torch.empty(2000, 512, 9)
+
+a[:, :, :, :] = all_p_img[:22199, :, :, :]
+b[:, :, :, :] = all_n_img[:22199, :, :, :]
+# c[:, :, :] = all_p_list[:2000, :, :]
+# d[:, :, :] = all_n_list[:2000, :, :]
+
+torch.save(a, data_dir + '/all_p_img' + '.pt')
+torch.save(b, data_dir + '/all_n_img' + '.pt')
+# torch.save(c, data_dir + '/all_p_list2000' + '.pt')
+# torch.save(d, data_dir + '/all_n_list2000' + '.pt')
--- train_simple.py
+++ train_simple.py
@@ -25,87 +25,58 @@ sam_file.close()
 
 hight = 224
 
-# all_p_img = torch.empty(22199, 3+3+4, hight, hight)
-# all_n_img = torch.empty(22199, 3+3+4, hight, hight)
+all_p_img = torch.empty(22199, 3+3+4, hight, hight)
+all_n_img = torch.empty(22199, 3+3+4, hight, hight)
 
-ap_list_index = [0]
-an_list_index = [0]
+# all_p_list = torch.empty(22199, 512, 9)
+# all_n_list = torch.empty(22199, 512, 9)
 
+index = 0
 
 # pool = Pool(2)
 for chromosome, chr_len in zip(chr_list, chr_length):
     print("======= deal " + chromosome + " =======")
     print("img start")
-    # t_positive_img = torch.load(data_dir + 'image/' + chromosome + '/positive_img' + '.pt') # 3
-    # t_negative_img = torch.load(data_dir + 'image/' + chromosome + '/negative_img' + '.pt')
-    # positive_img_mid = torch.load(data_dir + 'image/' + chromosome + '/positive_img_mid' + '.pt') # 3
-    # negative_img_mid = torch.load(data_dir + 'image/' + chromosome + '/negative_img_mid' + '.pt')
-    save_path = data_dir + 'image/' + chromosome
-    p_list_index = torch.load(save_path + '/positive_img_m(i)d.index' + '.pt')
-    n_list_index = torch.load(save_path + '/negative_img_m(i)d.index' + '.pt')
-    for i in range(len(p_list_index)):
-        p_list_index[i] = p_list_index[i] + ap_list_index[-1]
-    for i in range(len(n_list_index)):
-        n_list_index[i] = n_list_index[i] + an_list_index[-1]
-    ap_list_index = ap_list_index + p_list_index[1:]
-    # set_trace()
-    an_list_index = an_list_index + n_list_index[1:]
-    # positive_img_i_list = torch.load(save_path + '/positive_img_m(i)d' + '.pt')
-    # negative_img_i_list = torch.load(save_path + '/negative_img_m(i)d' + '.pt')
+    t_positive_img = torch.load(data_dir + 'image/' + chromosome + '/positive_img' + '.pt') # 3
+    t_negative_img = torch.load(data_dir + 'image/' + chromosome + '/negative_img' + '.pt')
+    positive_img_mid = torch.load(data_dir + 'image/' + chromosome + '/positive_img_mid' + '.pt') # 3
+    negative_img_mid = torch.load(data_dir + 'image/' + chromosome + '/negative_img_mid' + '.pt')
+    # positive_img_i = torch.load(data_dir + 'image/' + chromosome + '/positive_img_m(i)d' + '.pt')
+    # negative_img_i = torch.load(data_dir + 'image/' + chromosome + '/negative_img_m(i)d' + '.pt')
     print("img end")
     # img/positive_cigar_img
     # print("cigar start")
-    # positive_cigar_img = torch.load(data_dir + 'image/' + chromosome + '/positive_cigar_img' + '.pt') # 4
-    # negative_cigar_img = torch.load(data_dir + 'image/' + chromosome + '/negative_cigar_img' + '.pt')
+    positive_cigar_img = torch.load(data_dir + 'image/' + chromosome + '/positive_cigar_new_img' + '.pt') # 4
+    negative_cigar_img = torch.load(data_dir + 'image/' + chromosome + '/negative_cigar_new_img' + '.pt')
     # # positive_cigar_img = torch.cat([positive_cigar_img, positive_cigar_img6], 1)
     # # negative_cigar_img = torch.cat([negative_cigar_img, negative_cigar_img6], 1)
     # print("cigar end")
 
-    # length = len(positive_img_i_list)
+    length = len(positive_img_i)
+
+    all_p_list[index:index + length] = positive_img_i
+    all_n_list[index:index + length] = negative_img_i
+
+    all_p_img[index:index + length, :3, :, :] = t_positive_img
+    all_n_img[index:index + length, :3, :, :] = t_negative_img
+
+    # all_positive_img_mid[a2:a2 + length] = positive_img_mid
+    all_p_img[index:index + length, 3:6, :, :] = positive_img_mid
+    # all_negative_img_mid[b2:b2 + length] = negative_img_mid
+    all_n_img[index:index + length, 3:6, :, :] = negative_img_mid
 
 
-    # all_p_img[index:index + length, :3, :, :] = t_positive_img
-    # all_n_img[index:index + length, :3, :, :] = t_negative_img
-
-    # # all_positive_img_mid[a2:a2 + length] = positive_img_mid
-    # all_p_img[index:index + length, 3:6, :, :] = positive_img_mid
-    # # all_negative_img_mid[b2:b2 + length] = negative_img_mid
-    # all_n_img[index:index + length, 3:6, :, :] = negative_img_mid
+    # all_positive_cigar_img[a3:a3 + length] = positive_cigar_img
+    all_p_img[index:index + length, 6:, :, :] = positive_cigar_img
+    # all_negative_cigar_img[b3:b3 + length] = negative_cigar_img
+    all_n_img[index:index + length, 6:, :, :] = negative_cigar_img
 
 
-    # # all_positive_cigar_img[a3:a3 + length] = positive_cigar_img
-    # all_p_img[index:index + length, 6:, :, :] = positive_cigar_img
-    # # all_negative_cigar_img[b3:b3 + length] = negative_cigar_img
-    # all_n_img[index:index + length, 6:, :, :] = negative_cigar_img
+    all_positive_img_i_list[index:index + length] = positive_img_i_list
+    all_negative_img_i_list[index:index + length] = negative_img_i_list
 
+    index += length
 
-    # all_positive_img_i_list[index:index + length] = positive_img_i_list
-    # all_negative_img_i_list[index:index + length] = negative_img_i_list
-
-    # index += length
-
-all_p_list = torch.empty(ap_list_index[-1], 9)
-all_n_list = torch.empty(an_list_index[-1], 9)
-
-
-
-p_index = 0
-n_index = 0
-
-for i, (chromosome, chr_len) in enumerate(zip(chr_list, chr_length)):
-    save_path = data_dir + 'image/' + chromosome
-    positive_img_i_list = torch.load(save_path + '/positive_img_m(i)d' + '.pt')
-    negative_img_i_list = torch.load(save_path + '/negative_img_m(i)d' + '.pt')
-
-    plength = len(positive_img_i_list)
-    nlength = len(negative_img_i_list)
-
-
-    all_p_list[p_index:p_index + plength] = positive_img_i_list
-    all_n_list[n_index:n_index + nlength] = negative_img_i_list
-
-    p_index += plength
-    n_index += nlength
 
 # all_p_img = torch.cat([all_positive_img, all_positive_img_mid, all_positive_cigar_img], 1) # 3, 3, 3 + 6
 # all_n_img = torch.cat([all_negative_img, all_negative_img_mid, all_negative_cigar_img], 1)
@@ -120,11 +91,7 @@ for i, (chromosome, chr_len) in enumerate(zip(chr_list, chr_length)):
 
 print("finish")
 
-
-
-# torch.save(all_p_img, data_dir + '/all_p_img' + '.pt')
-# torch.save(all_n_img, data_dir + '/all_n_img' + '.pt')
-torch.save(all_p_list, data_dir + '/all_p_list' + '.pt')
-torch.save(all_n_list, data_dir + '/all_n_list' + '.pt')
-torch.save(ap_list_index, data_dir + '/all_p_list.index' + '.pt')
-torch.save(an_list_index, data_dir + '/all_n_list.index' + '.pt')
+torch.save(all_p_img, data_dir + '/all_p_img' + '.pt')
+torch.save(all_n_img, data_dir + '/all_n_img' + '.pt')
+# torch.save(all_p_list, data_dir + '/all_p_list' + '.pt')
+# torch.save(all_n_list, data_dir + '/all_n_list' + '.pt')
