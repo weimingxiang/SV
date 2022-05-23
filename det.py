be7eb72f936f537cec03cefcef31cee767d6b7e9
“weimingxiang” <1578270912@qq.com>
2022-05-23 17:09:01 +0800

    commit init 0

--- det.py
+++ det.py
@@ -1,13 +1,5 @@
 import torch
 import torchvision
 from pudb import set_trace
-set_trace()
-model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained=True)
 
-data_dir = "../datasets/NA12878_PacBio_MtSinai/"
-
-t_positive_img = torch.load(data_dir + 'image/' + "chr1" + '/positive_img' + '.pt') # 3
-
-re = model(t_positive_img[:10, :, :112, :112])
-
-re[0]
+model = torchvision.models.detection.retinanet_resnet50_fpn(num_classes = 2, pretrained_backbone=True, trainable_backbone_layers = 5)
